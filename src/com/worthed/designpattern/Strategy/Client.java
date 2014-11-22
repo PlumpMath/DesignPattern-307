7993de610c00aff3908126f0ecb57aafb4792a5a
Zhenguo <jingle1267@163.com>
2014-11-23 00:19:45 +0800

    [NewFeature] Add Strategy Pattern.

---
+++ src/com/worthed/designpattern/Strategy/Client.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2014 jinzhenguo1990@gmail.com
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.worthed.designpattern.Strategy;
+
+/**
+ * 策略模式(Strategy)是一种定义一系列算法的方法，从概念上来看，所有这些算法完成的都是相同的工作，
+ * 只是实现不同，它可以以相同的方式调用所有的算法，减少了各种算法类与使用算法类之间的耦合。
+ *
+ * Created by zhenguo on 11/22/14.
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        Context context;
+
+        context = new Context(new StrategyA());
+        context.contextInterface();
+
+        context = new Context(new StrategyB());
+        context.contextInterface();
+    }
+
+}
